04a724654d837019c8015263c14f2b1ec63e94ae
usama v <varikkottilusama@gmail.com>
2020-03-26 20:32:39 +0530

    add gcloud.sh to the home dir

--- installer.sh
+++ installer.sh
@@ -80,6 +80,8 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 sudo apt-get install apt-transport-https ca-certificates gnupg -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+echo "gcloud alpha cloud-shell ssh" > ~/gcloud.sh
+sudo chmod +x gcloud.sh
 gcloud init
 
 
